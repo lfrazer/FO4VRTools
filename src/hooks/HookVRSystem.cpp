99731258d5b2c16c038aabf3e123b77cfbdda058
L Frazer <lfrazer@github.com>
2020-05-18 20:38:17 -0400

    Update licenses

--- src/hooks/HookVRSystem.cpp
+++ src/hooks/HookVRSystem.cpp
@@ -1,3 +1,24 @@
+/*
+	FO4VRTools - VR Input plugin for mods of Fallout4 VR
+	Copyright (C) 2020 L Frazer
+	https://github.com/lfrazer
+
+	This file is part of FO4VRTools.
+
+	FO4VRTools is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	FO4VRTools is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with FO4VRTools.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "HookVRSystem.h"
 #include "../VRManager.h"
 #include "../ScaleformVR.h"
